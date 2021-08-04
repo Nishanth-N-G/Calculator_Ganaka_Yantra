27853bebf429e2211f0e04894e3e3ca949bad2ab
Nishanth-N-G <88285533+Nishanth-N-G@users.noreply.github.com>
2021-08-05 00:07:12 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,52 @@
+function dis(val)
+{
+  var currentResult= document.getElementById("result").value+=val;             
+}
+
+function onlyNumberKey(evt) {
+   var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+   if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+       return false;
+   return true;
+}
+
+function solve()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    if(y===undefined){
+        document.getElementById("result").value = 0;
+    }else if(y=== Infinity){
+        document.getElementById("result").value = "Math-error";    
+    }else if(isNaN(y)){
+        document.getElementById("result").value = "Syntax-error";
+    }else{
+        document.getElementById("result").value = y;   
+    }
+}
+
+function clr()
+{
+    document.getElementById("result").value = ""
+}
+
+function backspace() 
+   {
+       var value = document.getElementById("result").value;
+       document.getElementById("result").value = value.substr(0, value.length - 1);
+   }
+
+   function sqre()
+   {
+       solve();
+       var num= document.getElementById("result").value;
+       var square= num*num;
+       document.getElementById("result").value = square;
+   }
+   
+   function sqrt() 
+   {
+       solve();
+       var num=document.getElementById("result").value;
+       document.getElementById("result").value = Math.sqrt(num);
+   }
